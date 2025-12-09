e7dbc1842a8e0a1ecf72a88ea27da71448d3e6e2
Rayonnant nyonsa <nyonsaakasha@gmail.com>
2025-12-09 10:07:02 +0630

    Import footer in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import Banner from './Banner';
 //import Cart from './Cart';
 import ShoppingList from './ShoppingList';
+import Footer from './Footer';
 import '../styles/App.css';
+
+
 
 function App() {
   return <div>
          <Banner />
-      {/*<Cart/>*/}
-      <ShoppingList />
+         {/*<Cart/>*/}
+        <ShoppingList />
+      <Footer />
         </div>;
 }
 
