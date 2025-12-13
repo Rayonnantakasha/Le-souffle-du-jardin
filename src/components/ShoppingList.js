80f8ad60c57eac83981ad51c22270a37ebb529ec
Rayonnant nyonsa <nyonsaakasha@gmail.com>
2025-12-13 07:48:34 +0630

    ajouts des commentaires dans shoppingList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,25 @@
+/* eslint-disable no-lone-blocks */
 import { useState } from 'react'
 import { plantList } from '../Datas/plantList'
 import PlantItem from './PlantItm'
 import Categories from './Categories'
 import '../styles/ShoppingList.css'
 
+
 function ShoppingList({ cart, updateCart }) {
+    //Fonction pour recupérer et lister les Categories se trouvant dans PlantList
     const [activeCategory, setActiveCategory] = useState('')
     const categories = plantList.reduce(
         (acc, plant) =>
             acc.includes(plant.category) ? acc : acc.concat(plant.category),
         []
     )
-
+    
+    //Fonction pour Ajouter les élements aux panier
     function addToCart(name, price) {
-        const currentPlantSaved = cart.find((plant) => plant.name === name)
+        const currentPlantSaved = cart.find((plant) => plant.name === name) //verifie si la plante est déjà dans le panier
+
+        //Si elle existe, on enlève l’ancienne version pour la remplacer par une copie avec +1 quantité.
         if (currentPlantSaved) {
             const cartFilteredCurrentPlant = cart.filter(
                 (plant) => plant.name !== name
@@ -29,6 +35,7 @@ function ShoppingList({ cart, updateCart }) {
 
     return (
         <div className='lmj-shopping-list'>
+            {/*hoppingList passe des données à Categories (props descendant).*/}
             <Categories
                 categories={categories}
                 setActiveCategory={setActiveCategory}
@@ -36,6 +43,7 @@ function ShoppingList({ cart, updateCart }) {
             />
 
             <ul className='lmj-plant-list'>
+                {/*Si aucune catégorie sélectionnée → tout afficher, Sinon → afficher uniquement les plantes correspondant à activeCategory*/}
                 {plantList.map(({ id, cover, name, water, light, price, category }) =>
                     !activeCategory || activeCategory === category ? (
                         <div key={id}>
