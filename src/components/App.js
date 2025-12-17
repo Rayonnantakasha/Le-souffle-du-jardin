b71e54f07a0a4b89aeee756301e7cef95d7500ac
Rayonnant nyonsa <nyonsaakasha@gmail.com>
2025-12-17 11:58:07 +0630

    Feat : ajout des commentaire et parametre pour sauvegarder le panier si on rafraichi la page

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,20 @@ import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 import '../styles/Layout.css'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 
-
+/*
+Mémoriser le panier dans le navigateur grâce à localStorage
+Recharger le panier au démarrage de l’application
+Sauvegarder le panier à chaque modification
+ */
 
 function App() {
-    const [cart, updateCart] = useState([])
+    const savedCart = localStorage.getItem('cart') //On lit dans le stockage du navigateur
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : []) //initialiser le state du panier avec les données récupérées
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart)) //sauvegarder le panier à chaque modification
+	}, [cart])
     return (
         <div>
             <Banner />
