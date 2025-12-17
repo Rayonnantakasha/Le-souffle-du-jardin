0258d2a7e268313f2b41f93ce59f74c321e2e02f
Rayonnant nyonsa <nyonsaakasha@gmail.com>
2025-12-17 11:58:30 +0630

    Ajout d'un useEffet

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import '../styles/Cart.css'
 
-function Cart({ cart, updateCart }) {
+function Cart({ cart, updateCart}) {
     const [isOpen, setIsOpen] = useState(true)
     const total = cart.reduce(
         (acc, plantType) => acc + plantType.amount * plantType.price,
         0
     )
+    useEffect(() => {
+      document.title = `LMJ: ${total}€ d'achats`
+    },[total])
     return isOpen ? (
         <div className='lmj-cart'>
             <button
