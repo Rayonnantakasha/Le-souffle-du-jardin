a2c68add71bb63968320ee71824595097a5ff5ad
Rayonnant nyonsa <nyonsaakasha@gmail.com>
2025-12-23 20:17:09 +0630

    Add react snowfall

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Snowfall from 'react-snowfall'
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -7,6 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Snowfall 
+     color = "#9fe3b2"
+    />
     <App />
   </React.StrictMode>
 );
